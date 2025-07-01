6f0b8285f7eebb8d9cb9b92d203d2d5567379074
Peter Kovalets <peterkovalets@proton.me>
2025-07-01 18:17:23 +0700

    Добавлен функционал для работы с камерой

--- src/main/java/org/peterkovalets/app/App.java
+++ src/main/java/org/peterkovalets/app/App.java
@@ -1,12 +1,40 @@
 package org.peterkovalets.app;
 
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.peterkovalets.app.components.ImageLabel;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Основной класс приложения.
  */
-public class App {
+public class App extends JFrame {
+
+  private static final int CAMERA_ID = 0;
+
+  private final Mat imageMatrix;
+  private final Camera camera;
+
+  /**
+   * Конструктор класса.
+   */
+  public App() {
+    super("OpenCV Project");
+
+    imageMatrix = new Mat();
+    ImageLabel imageLabel = new ImageLabel();
+    camera = new Camera(imageMatrix, imageLabel, CAMERA_ID);
+    getContentPane().add(BorderLayout.CENTER, imageLabel);
+
+    setUpBottomPanel();
+
+    setSize(800, 600);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+  }
+
   /**
    * Точка входа в приложение.
    *
@@ -14,7 +42,40 @@ public class App {
    */
   public static void main(String[] args) {
     nu.pattern.OpenCV.loadLocally();
-    Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-    System.out.println("mat = " + mat.dump());
+    new App();
+  }
+
+  /**
+   * Создает нижнюю панель пользовательского интерфейса.
+   */
+  private void setUpBottomPanel() {
+    JPanel bottomPanel = new JPanel();
+    JButton loadImageBtn = new JButton("Загрузить изображение");
+    JButton startCameraBtn = new JButton("Включить камеру");
+    JButton cameraScreenshotBtn = new JButton("Получить изображение с камеры");
+
+    loadImageBtn.addActionListener(event -> {
+      if (camera.getIsCapturing()) {
+        showCapturingWarningDialog();
+        return;
+      }
+
+      // Загрузить изображение
+    });
+    startCameraBtn.addActionListener(event -> camera.start());
+    cameraScreenshotBtn.addActionListener(event -> camera.stop());
+
+    bottomPanel.add(loadImageBtn);
+    bottomPanel.add(startCameraBtn);
+    bottomPanel.add(cameraScreenshotBtn);
+    getContentPane().add(BorderLayout.SOUTH, bottomPanel);
+  }
+
+  /**
+   * Показывает предупреждение о том, что камера уже работает.
+   */
+  private void showCapturingWarningDialog() {
+    JOptionPane.showMessageDialog(null, "Камера должна быть остановлена!",
+        "Камера", JOptionPane.WARNING_MESSAGE);
   }
 }
---
+++ src/main/java/org/peterkovalets/app/Camera.java
@@ -0,0 +1,85 @@
+package org.peterkovalets.app;
+
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+import org.peterkovalets.app.components.ImageLabel;
+
+import javax.swing.*;
+
+/**
+ * Класс камеры.
+ */
+public class Camera {
+
+  private VideoCapture capture;
+  private final Mat imageMatrix;
+  private final ImageLabel imageLabel;
+  private final int cameraId;
+  private boolean isCapturing = false;
+
+  /**
+   * Конструктор класса.
+   *
+   * @param imageMatrix матрица изображения
+   * @param imageLabel компонент изображения
+   * @param cameraId идентификатор камеры
+   */
+  public Camera(Mat imageMatrix, ImageLabel imageLabel, int cameraId) {
+    this.imageMatrix = imageMatrix;
+    this.imageLabel = imageLabel;
+    this.cameraId = cameraId;
+  }
+
+  /**
+   * Запускает камеру.
+   */
+  public void start() {
+    if (isCapturing) {
+      JOptionPane.showMessageDialog(null, "Камера уже работает!",
+          "Камера", JOptionPane.WARNING_MESSAGE);
+      return;
+    }
+
+    Runnable task = () -> {
+      if (capture == null) {
+        capture = new VideoCapture();
+      }
+      isCapturing = capture.open(cameraId);
+
+      if (!isCapturing) {
+        JOptionPane.showMessageDialog(null, "Не удалось получить доступ к камере!",
+            "Ошибка", JOptionPane.ERROR_MESSAGE);
+      }
+
+      while (isCapturing) {
+        capture.read(imageMatrix);
+        imageLabel.drawImage(imageMatrix);
+      }
+
+      capture.release();
+    };
+    new Thread(task).start();
+  }
+
+  /**
+   * Останавливает камеру.
+   */
+  public void stop() {
+    if (!isCapturing) {
+      JOptionPane.showMessageDialog(null, "Камера не включена!",
+          "Камера", JOptionPane.WARNING_MESSAGE);
+      return;
+    }
+
+    isCapturing = false;
+  }
+
+  /**
+   * Возвращает булево значение захвата камеры.
+   *
+   * @return булево значение, обозначающее работает ли камера
+   */
+  public boolean getIsCapturing() {
+    return isCapturing;
+  }
+}
